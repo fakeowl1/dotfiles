c5bad16be9b47f97fe287206c2bdb7c06cc3e17c
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-09-03 00:49:21 +0300

    delete unnecessary scripts

--- .config/scripts/jazz.sh
+++
@@ -1,3 +0,0 @@
-url="https://youtu.be/XnzxEGLaPxs"
-
-pkill -f $url || mpv --shuffle --no-video "$url"
--- .config/scripts/lofi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-url="https://youtu.be/5qap5aO4i9A"
-
-pkill -f $url || mpv --shuffle --no-video "$url" 
